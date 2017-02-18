72bb15eacfdb75349833dc1ff364e2cfdd328aa1
ASHL7 <ashl_777@yahoo.com>
2017-02-17 20:32:22 -0800

    every 10th item of list is a row of categories

--- app/src/main/java/com/ashl7developer/cityexplore/VenueListAdapter.java
+++ app/src/main/java/com/ashl7developer/cityexplore/VenueListAdapter.java
@@ -20,12 +20,29 @@ import model.Venue;
 public class VenueListAdapter extends ArrayAdapter<Venue> {
 
     List<Venue> venueList;
+    // row layout types
+    private static final int REGULAR_ITEM_TYPE = 0;
+    private static final int CATEGORY_ITEM_TYPE = 1;
+
 
     public VenueListAdapter(Context context, int resource, List<Venue> objects) {
         super(context, resource, objects);
         venueList = objects;
     }
 
+
+    // returns number of different layouts for row items
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    // returns type of the layout based on the position of the item on the list
+    // TODO: first time is every 10 items, but then goes to every 9th item, fix it!
+    @Override
+    public int getItemViewType(int position) {
+        return (position != 0 && position%10 == 0) ? CATEGORY_ITEM_TYPE : REGULAR_ITEM_TYPE;
+    }
 
     // This method indicates how the listView should look
     @Override
@@ -35,34 +52,38 @@ public class VenueListAdapter extends ArrayAdapter<Venue> {
             // TODO what to show when there is no element
             return convertView;
         }
-
+        int layoutType = getItemViewType(position);
         Venue venue = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.item_venue_list, parent, false);
+            switch (layoutType) {
+                case REGULAR_ITEM_TYPE:
+                    convertView = LayoutInflater.from(getContext()).inflate(
+                            R.layout.item_venue_list, parent, false);
+                    TextView nameTextView = (TextView) convertView.findViewById(R.id.name_textview);
+                    nameTextView.setText(venue.getName().toString());
+
+                    TextView locationTextView = (TextView) convertView.findViewById(R.id.location_textview);
+                    locationTextView.setText(venue.getLocation().getAddress());
+
+                    List<Category> categories = venue.getCategories();
+                    StringBuffer sb = new StringBuffer();
+                    for(Category category: categories) sb.append(category.getName() + ", ");
+                    sb.delete(sb.length()-2, sb.length());
+                    TextView categoryTextView = (TextView) convertView.findViewById(R.id.category_textview);
+                    categoryTextView.setText(sb.toString());
+
+                    ImageView thumbnailImageView = (ImageView) convertView.findViewById(R.id.thumbnail_imageview);
+                    thumbnailImageView.setImageResource(R.drawable.unknown_image);
+
+                    break;
+                case CATEGORY_ITEM_TYPE:
+                    convertView = LayoutInflater.from(getContext()).inflate(
+                            R.layout.item_categories_list, parent, false);
+                    break;
+            }
+
         }
-
-        // if we are at the
-        if(position%50 == 1 && position != 1){
-
-        }
-
-        TextView nameTextView = (TextView) convertView.findViewById(R.id.name_textview);
-        nameTextView.setText(venue.getName().toString());
-
-        TextView locationTextView = (TextView) convertView.findViewById(R.id.location_textview);
-        locationTextView.setText(venue.getLocation().getAddress());
-
-        List<Category> categories = venue.getCategories();
-        StringBuffer sb = new StringBuffer();
-        for(Category category: categories) sb.append(category.getName() + ", ");
-        sb.delete(sb.length()-2, sb.length());
-        TextView categoryTextView = (TextView) convertView.findViewById(R.id.category_textview);
-        categoryTextView.setText(sb.toString());
-
-        ImageView thumbnailImageView = (ImageView) convertView.findViewById(R.id.thumbnail_imageview);
-        thumbnailImageView.setImageResource(R.drawable.unknown_image);
 
         return convertView;         // Return the completed view to render on screen
     }
